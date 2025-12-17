86e65b310b8e90a5e4a3621fa767e61c8d12dcda
CloudMW <mfy.2000@outlook.com>
2025-12-17 10:19:54 +0800

    gtest

--- demo/demo_XX/main.cpp
+++ demo/demo_XX/main.cpp
@@ -3,3 +3,7 @@
 //
 
 //#include "pcdl/debug/pcdl_debug.hpp"
+
+#include <gtest/gtest.h>
+
+ASSERT_QE(5,2+3);
---
+++ demo/gtest/README.md
@@ -0,0 +1,4 @@
+# Gtest
+# 资料
+https://google.github.io/googletest/
+https://ggdocs.cn/googletest/
